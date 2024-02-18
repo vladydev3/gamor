32d5ff8c16e56eafd728fd34b00b82ead33de23f
Vladimir Pinera <vladypv3@gmail.com>
2024-02-18 16:02:51 -0500

    navbar small changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link';
 import ThemeSwitch from './ThemeSwitch';
 import { useEffect, useState } from 'react';
 import {supabase} from '../supabaseClient';
@@ -32,28 +33,30 @@ const NavBar = () => {
     }
 
     return (
-            <nav className="cursor-default flex justify-between px-20 py-12 ">
-                <ul className="flex gap-8">
-                    <li className={`transition duration-500 hover:text-violet-600 dark:hover:text-orange-500
-                    ${pathname === '/' ? 'dark:text-orange-500 text-violet-600' : 'dark:text-white text-black'}
-                    `}><a href="/">Home</a></li>
-                    <li><a href="#" className="transition duration-500 hover:text-orange-500">Streams</a></li>
-                    <li><a href="#" className="transition duration-500 hover:text-orange-500">Party</a></li>
-                    <li><a href="#" className="transition duration-500 hover:text-orange-500 ">Premium</a></li>
-                </ul>
-                <ul className="flex gap-10">
-                    {user ? (
-                        <li><a onClick={handleLogout} className="cursor-pointer transition duration-500 hover:text-orange-500">Sign Out</a></li>
-                    ) : (
-                        <>
-                            <li><a href="/login" className="transition duration-500 hover:text-orange-500">Sign In</a></li>
-                            <li><a href="/register" className="transition duration-500 hover:text-orange-500 border-2 border-gray-600 bg-black-100 shadow-md shadow-black text-white rounded-full py-2 px-4">Create account</a></li>
-                        </>
-                    )}
-                    <ThemeSwitch />
-                </ul>
-            </nav>
-        )
+        <nav className="cursor-default flex justify-between px-20 py-12 ">
+        <ul className="flex gap-8">
+            <li className={`transition duration-500 hover:text-violet-600 dark:hover:text-orange-500
+            ${pathname === '/' ? 'dark:text-orange-500 text-violet-600' : 'dark:text-white text-black'}
+            `}>
+            <Link href="/">Home</Link>
+            </li>
+            <li><Link className='transition duration-500 hover:text-violet-600 dark:hover:text-orange-500' href="#">Streams</Link></li>
+            <li><Link className='transition duration-500 hover:text-violet-600 dark:hover:text-orange-500' href="#">Party</Link></li>
+            <li><Link className='transition duration-500 hover:text-violet-600 dark:hover:text-orange-500' href="#">Premium</Link></li>
+        </ul>
+        <ul className="flex gap-10">
+            {user ? (
+            <li><a onClick={handleLogout} className="cursor-pointer transition duration-500 hover:text-orange-500">Sign Out</a></li>
+            ) : (
+            <>
+                <li><Link className='transition duration-500 hover:text-violet-600 dark:hover:text-orange-500' href="/login">Sign In</Link></li>
+                <li><Link href="/register" className="transition duration-500 hover:text-orange-500 border-2 border-gray-600 bg-black-100 shadow-md shadow-black text-white rounded-full py-2 px-4">Create account</Link></li>
+            </>
+            )}
+            <ThemeSwitch />
+        </ul>
+        </nav>
+    )
 };
 
 export default NavBar;
