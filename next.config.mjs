e9141852cc4c3a86a744403d9f4cf8d1877755e8
Vladimir Pinera <vladypv3@gmail.com>
2024-02-18 16:34:41 -0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     env: {
         SUPABASE_URL: process.env.SUPABASE_URL,
-        SUPABASE_KEY: process.env.SUPABASE_KEY,
+        SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
     },
 };
 
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://kwihwbcmhqkwcqefxtrl.supabase.co'
-const supabaseAnonKey = process.env.SUPABASE_KEY
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
